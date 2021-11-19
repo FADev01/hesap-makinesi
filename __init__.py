970a6d6b33266d67ecd13c90462e95b7f6da6045
FADev01 <94678219+FADev01@users.noreply.github.com>
2021-11-19 11:49:55 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,13 @@
+def toplama(num1,num2):
+    return num1+num2
+
+def çıkarma(num1,num2):
+    return num1-num2
+
+def çarpma(num1,num2):
+    return num1*num2
+
+def bölme(num1,num2):
+    return num1/num2
+
+print(çarpma(4,7))
